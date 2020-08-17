236416790ea03f68952d729d5c28b50c4718158c
dnzaldg <daladag41@gmail.com>
2020-08-17 21:25:29 +0300

    First commit

---
+++ src/test/java/com/testinium/StepImplementation.java
@@ -0,0 +1,71 @@
+package com.testinium;
+
+
+import com.testinium.base.BaseTest;
+import com.thoughtworks.gauge.Step;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class StepImplementation extends BaseTest {
+
+    @Step("start")
+    public void start()
+    {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.android.packageinstaller:id/permission_allow_button")));
+        appiumDriver.findElement(By.id("com.android.packageinstaller:id/permission_allow_button")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frPrivacy_btnAccept")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frPrivacy_btnAccept")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/acMain_btnBooking")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/acMain_btnBooking")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_tvOneWay")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_tvOneWay")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_llFrom")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_llFrom")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frAirportSelection_etSearch")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frAirportSelection_etSearch")).sendKeys("İstanbul");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/itemAirport_tvBottom")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/itemAirport_tvBottom")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_llTo")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_llTo")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frAirportSelection_etSearch")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frAirportSelection_etSearch")).sendKeys("Ankara");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/itemAirport_tvBottom")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/itemAirport_tvBottom")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_rlDeparture")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_rlDeparture")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_calendarPickerView")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_calendarPickerView")).getText();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.turkishairlines.mobile:id/frDashboard_btnDone")));
+        appiumDriver.findElement(By.id("com.turkishairlines.mobile:id/frDashboard_btnDone")).click();
+
+    }
+
+    public void getToDay()
+    {
+        Date date=new Date();
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("Turkey/İstanbul"));
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+    }
+
+}
---
+++ src/test/java/com/testinium/base/BaseTest.java
@@ -0,0 +1,37 @@
+package com.testinium.base;
+
+import com.thoughtworks.gauge.AfterSuite;
+import com.thoughtworks.gauge.BeforeSuite;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+    public static AppiumDriver appiumDriver;
+    public static WebDriverWait wait;
+
+    @BeforeSuite
+    public void initializeDriver() throws MalformedURLException {
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
+        desiredCapabilities.setCapability(MobileCapabilityType.VERSION, "8.0");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.turkishairlines.mobile");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.turkishairlines.mobile.ui.ACSplash");
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
+        appiumDriver = new AndroidDriver(url, desiredCapabilities);
+        wait=new WebDriverWait(appiumDriver,30);
+    }
+
+    @AfterSuite
+    public void closeDriver(){
+        appiumDriver.quit();
+    }
+}
